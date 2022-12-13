f9860cd4591cff314823c9b283036db7dadf486d
ACh1cken <68433261+ACh1cken@users.noreply.github.com>
2022-12-13 22:14:06 +0800

    Changes to initial algorithm and regerate p and q instead of e

--- KA.java
+++ KA.java
@@ -1,48 +1,69 @@
 import java.io.DataInputStream;
 import java.io.IOException;
 import java.math.BigInteger;
+import java.security.SecureRandom;
 import java.util.Random;
 
 //This project Made by (Khaled Androu, Chris, Nicholas,Humayun, Yee and Ali).
 
- class KA
+ public class KA
 {
-     private BigInteger p1;              //prime1
-      private BigInteger q2;             //prime2
-       private BigInteger M;            //modulus
-        private BigInteger totient;     //totient
-         private BigInteger e;          //public exponent
-          private BigInteger d;         //private exponent
-           private Random     ra;
-            private int bitlength = 1024;
+    private BigInteger p1;              //prime1
+    private BigInteger q2;             //prime2
+    private BigInteger M;            //modulus
+    private BigInteger phi;     //totient
+    private static final BigInteger ENCRYPTION_EXPONENT = BigInteger.valueOf(65537);          //public exponent
+    private BigInteger d;         //private exponent
+    private int bitlength = 512; //512 length for p and q
+
 
     public KA()
     {
-         ra = new Random();
-          p1 = BigInteger.probablePrime(bitlength, ra);
-           q2 = BigInteger.probablePrime(bitlength, ra);
-            M = p1.multiply(q2);
-             totient = p1.subtract(BigInteger.ONE).multiply(q2.subtract(BigInteger.ONE));
+        //prevent reuse of random number
+          p1 = BigInteger.probablePrime(bitlength, new SecureRandom());
+          q2 = BigInteger.probablePrime(bitlength, new SecureRandom());
+          M = p1.multiply(q2);
+          phi = p1.subtract(BigInteger.ONE).multiply(q2.subtract(BigInteger.ONE));
 
         // Set e to a specific value rather than generating it randomly
-        e = BigInteger.valueOf(65537);
 
+
+        /*
          // Check whether e is relatively prime to totient(n)
-    if (totient.gcd(e).compareTo(BigInteger.ONE) != 0) {
+    if (phi.gcd(e).compareTo(BigInteger.ONE) != 0) {
 
         // If not, choose a different value for e that is relatively prime to totient(n)
         e = BigInteger.probablePrime(bitlength / 2, ra);
-        while (totient.gcd(e).compareTo(BigInteger.ONE) > 0 && e.compareTo(totient) < 0)
+        while (phi.gcd(e).compareTo(BigInteger.ONE) > 0 && e.compareTo(totient) < 0)
         {
             e.add(BigInteger.ONE);
         }
     }
-    d = e.modInverse(totient);
+    d = e.modInverse(phi);
+         */
+
+        // Use an encryption exponent e = 65537 and ensure that it is relatively prime to phi(n)
+        while (!phi.gcd(ENCRYPTION_EXPONENT).equals(BigInteger.ONE)){
+            // If it is not, go back to Step 1 and generate new values for p and q
+            p1 = BigInteger.probablePrime(bitlength, new SecureRandom());
+            q2 = BigInteger.probablePrime(bitlength, new SecureRandom());
+            M = p1.multiply(q2);
+            phi = p1.subtract(BigInteger.ONE).multiply(q2.subtract(BigInteger.ONE));
+        }
+// Compute the value for the decryption exponent d, which is the multiplicative inverse of e mod phi(n)
+         d = ENCRYPTION_EXPONENT.modInverse(phi);
+        System.out.println("P "+p1.toString());
+        System.out.println("Q "+q2.toString());
+        System.out.println("M "+M.toString());
+        System.out.println("Phi "+phi.toString());
+        System.out.println("D "+d.toString());
+
+
 }
 
-    public KA(BigInteger e, BigInteger d, BigInteger M)
+    public KA(BigInteger d, BigInteger M)
     {
-         this.e = e;
+
           this.d = d;
            this.M = M;
     }
@@ -50,14 +71,14 @@ import java.util.Random;
     @SuppressWarnings("deprecation")
     public static void main(String[] args) throws IOException
     {
-         KA rsa = new KA();
-          DataInputStream in = new DataInputStream(System.in);
-           String teststring;
-            System.out.println("Enter the plain text:");
-             teststring = in.readLine();
-              System.out.println("Encrypting String: " + teststring);
-               System.out.println("String in Bytes: "
-                 + bytesToString(teststring.getBytes()));
+        KA rsa = new KA();
+        DataInputStream in = new DataInputStream(System.in);
+        String teststring;
+        System.out.println("Enter the plain text:");
+        teststring = in.readLine();
+        System.out.println("Encrypting String: " + teststring);
+        System.out.println("String in Bytes: "
+                + bytesToString(teststring.getBytes()));
 
         // encrypt
         byte[] encrypted = rsa.encrypt(teststring.getBytes());
@@ -65,8 +86,8 @@ import java.util.Random;
         // decrypt
         byte[] decrypted = rsa.decrypt(encrypted);
 
-         System.out.println("Decrypting Bytes: " + bytesToString(decrypted));
-          System.out.println("Decrypted String: " + new String(decrypted));
+        System.out.println("Decrypting Bytes: " + bytesToString(decrypted));
+        System.out.println("Decrypted String: " + new String(decrypted));
     }
 
     private static String bytesToString(byte[] encrypted)
@@ -83,7 +104,7 @@ import java.util.Random;
     public byte[] encrypt(byte[] message)
     {
 
-        return (new BigInteger(message)).modPow(e, M).toByteArray();
+        return (new BigInteger(message)).modPow(ENCRYPTION_EXPONENT, M).toByteArray();
     }
 
     // Decrypt message
